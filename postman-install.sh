4453252a7cb434b956df5de640d17677f5dd5a76
Rabindra Joshi <mail@rjoshi.net>
2020-01-25 10:15:10 +0545

    Add postman install script
    Ubuntu Software store always has an older version.
    Gets latest version.
    Creates a desktop entry.

---
+++ postman-install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Get postman app
+wget https://dl.pstmn.io/download/latest/linux64 -O postman.tar.gz
+sudo tar -xzf postman.tar.gz -C /opt
+sudo ln -s /opt/Postman/Postman /usr/bin/postman
+
+#Create a Desktop Entry
+cat > ~/.local/share/applications/postman.desktop <<EOL
+[Desktop Entry]
+Encoding=UTF-8
+Name=Postman
+Exec=postman
+Icon=/opt/Postman/app/resources/app/assets/icon.png
+Terminal=false
+Type=Application
+Categories=Development;
+EOL
